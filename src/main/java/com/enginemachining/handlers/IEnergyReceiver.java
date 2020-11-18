9c4bdc8b5b55e0b5d09ae7e5d7b0875fe3cd0bfe
patryk3211 <patrykmierzy@gmail.com>
2020-11-18 12:00:59 +0100

    Added Energy Interfaces

---
+++ src/main/java/com/enginemachining/handlers/IEnergyReceiver.java
@@ -0,0 +1,7 @@
+package com.enginemachining.handlers;
+
+import net.minecraftforge.energy.IEnergyStorage;
+
+public interface IEnergyReceiver {
+    IEnergyStorage getHandler();
+}
---
+++ src/main/java/com/enginemachining/handlers/IEnergySender.java
@@ -0,0 +1,7 @@
+package com.enginemachining.handlers;
+
+import net.minecraftforge.energy.IEnergyStorage;
+
+public interface IEnergySender {
+    IEnergyStorage getHandler();
+}
