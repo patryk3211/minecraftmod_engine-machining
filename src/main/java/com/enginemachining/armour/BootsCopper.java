8cf824e9c425cb1c80dc9771a988c5e6cf7b2722
Subclo <marcel.pawlak2007@gmail.com>
2021-06-11 18:22:14 +0200

    Tools xd.

--- src/main/java/com/enginemachining/armour/BootsCopper.java
+++ src/main/java/com/enginemachining/armour/BootsCopper.java
@@ -1,11 +1,21 @@
 package com.enginemachining.armour;
 
 import com.enginemachining.items.ModdedItemGroups;
+import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nullable;
 
 public class BootsCopper extends ArmorItem {
     public BootsCopper() {
         super(ModArmorMaterial.copper, EquipmentSlotType.FEET, new Properties().tab(ModdedItemGroups.tools));
+    }
+
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return "enginemachining:item/armor/armor_copper.png";
     }
 }
--- src/main/java/com/enginemachining/armour/LegginsCopper.java
+++ src/main/java/com/enginemachining/armour/LegginsCopper.java
@@ -1,11 +1,20 @@
 package com.enginemachining.armour;
 
 import com.enginemachining.items.ModdedItemGroups;
+import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nullable;
 
 public class LegginsCopper extends ArmorItem {
     public LegginsCopper() {
         super(ModArmorMaterial.copper, EquipmentSlotType.LEGS, new Properties().tab(ModdedItemGroups.tools));
+    }
+    @Nullable
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+        return "enginemachining:textures/item/armor/armor_copper.png";
     }
 }
