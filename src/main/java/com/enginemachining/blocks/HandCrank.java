a8f8eaf05684aaf1f0db998e9805f35b696886f3
patryk3211 <patrykmierzy@gmail.com>
2021-06-25 14:45:59 +0200

    Hand Crank shape adjusted and moved to final variables

--- src/main/java/com/enginemachining/blocks/HandCrank.java
+++ src/main/java/com/enginemachining/blocks/HandCrank.java
@@ -25,6 +25,14 @@ import javax.annotation.Nullable;
 import java.util.Random;
 
 public class HandCrank extends Block {
+    private static final VoxelShape NORTH = VoxelShapes.or(Block.box(6.5, 6.5, 0, 9.5, 9.5, 3), Block.box(1, 1, 3, 15, 15, 6)).optimize();
+    private static final VoxelShape SOUTH = VoxelShapes.or(Block.box(6.5, 6.5, 16, 9.5, 9.5, 13), Block.box(1, 1, 13, 15, 15, 10)).optimize();
+    private static final VoxelShape WEST = VoxelShapes.or(Block.box(0, 6.5, 6.5, 3, 9.5, 9.5), Block.box(3, 1, 1, 6, 15, 15));
+    private static final VoxelShape EAST = VoxelShapes.or(Block.box(16, 6.5, 6.5, 13, 9.5, 9.5), Block.box(13, 1, 1, 10, 15, 15));
+    private static final VoxelShape DOWN = VoxelShapes.or(Block.box(6.5, 0, 6.5, 9.5, 3, 9.5), Block.box(1, 3, 1, 15, 6, 15));
+    private static final VoxelShape UP = VoxelShapes.or(Block.box(6.5, 16, 6.5, 9.5, 13, 9.5), Block.box(1, 13, 1, 15, 10, 15));
+
+
     public HandCrank() {
         super(Properties.of(Material.WOOD));
     }
@@ -43,12 +51,12 @@ public class HandCrank extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader block, BlockPos pos, ISelectionContext context) {
         switch (state.getValue(BlockStateProperties.FACING)) {
-            case NORTH: return VoxelShapes.or(Block.box(7, 7, 0, 9, 9, 3), Block.box(1, 1, 3, 15, 15, 6));
-            case SOUTH: return VoxelShapes.or(Block.box(7, 7, 16, 9, 9, 13), Block.box(1, 1, 13, 15, 15, 10));
-            case WEST: return VoxelShapes.or(Block.box(0, 7, 7, 3, 9, 9), Block.box(3, 1, 1, 6, 15, 15));
-            case EAST: return VoxelShapes.or(Block.box(16, 7, 7, 13, 9, 9), Block.box(13, 1, 1, 10, 15, 15));
-            case DOWN: return VoxelShapes.or(Block.box(7, 0, 7, 9, 3, 9), Block.box(1, 3, 1, 15, 6, 15));
-            case UP: return VoxelShapes.or(Block.box(7, 16, 7, 9, 13, 9), Block.box(1, 13, 1, 15, 10, 15));
+            case NORTH: return NORTH;
+            case SOUTH: return SOUTH;
+            case WEST: return WEST;
+            case EAST: return EAST;
+            case DOWN: return DOWN;
+            case UP: return UP;
             default: return VoxelShapes.block();
         }
     }
